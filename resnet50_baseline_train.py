67cbdb9702fdb5034fa55b1a25496a5e362913b6
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2023-03-27 10:23:03 +0800

    initial

--- resnet50_baseline_train.py
+++ resnet50_baseline_train.py
@@ -56,9 +56,11 @@ class ResNet50(LightningModule):
         return [optimizer], [scheduler]
 
     def train_dataloader(self):
+        hflip_prob=0.5
+        interpolation=InterpolationMode.BILINEAR
         transform = transforms.Compose([
-            transforms.Resize(256),
-            transforms.CenterCrop(224),
+            transforms.RandomResizedCrop(224, interpolation=interpolation),
+            transforms.RandomHorizontalFlip(hflip_prob),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ])
@@ -66,8 +68,9 @@ class ResNet50(LightningModule):
         return torch.utils.data.DataLoader(dataset, batch_size=self.batch_size, num_workers=8, pin_memory=True)
 
     def val_dataloader(self):
+        interpolation=InterpolationMode.BILINEAR
         transform = transforms.Compose([
-            transforms.Resize(256),
+            transforms.Resize(256,interpolation=interpolation),
             transforms.CenterCrop(224),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
