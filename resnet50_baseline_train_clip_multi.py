f6e8e81f67d43637c5c44fe51c8b4c877eba0dcc
lwz <905328134@qq.com>
2023-03-28 19:36:03 +0800

    initial

--- resnet50_baseline_train_clip_multi.py
+++ resnet50_baseline_train_clip_multi.py
@@ -5,14 +5,40 @@ from torch import nn
 import torchvision
 import torchvision.transforms as transforms
 from torchvision.transforms.functional import InterpolationMode
+import pytorch_lightning as pl
 from pytorch_lightning import LightningModule, Trainer
 from pytorch_lightning.loggers import WandbLogger
 from pytorch_lightning.callbacks import ModelCheckpoint, LearningRateMonitor
 from pl_bolts.optimizers.lr_scheduler import LinearWarmupCosineAnnealingLR
 from torch.optim.lr_scheduler import StepLR
 from args import parse_args
-import pytorch_lightning as pl
 import torch.nn.functional as F
+
+
+class MultiScaleNet(nn.Module):
+    def __init__(self):
+        super(MultiScaleNet, self).__init__()
+        self.u_net = torchvision.models.resnet50(pretrained=False)
+        self.small_size = (32, 32)
+        self.mid_size = (128, 128)
+        self.large_size = (224, 224)
+
+    def forward(self, x):
+        small_imgs = F.interpolate(x, size=self.small_size, mode='bilinear')
+        mid_imgs = F.interpolate(x, size=self.mid_size, mode='bilinear')
+        large_imgs = F.interpolate(x, size=self.large_size, mode='bilinear')
+
+        small_imgs = F.interpolate(small_imgs, size=self.large_size, mode='bilinear')
+        mid_imgs = F.interpolate(mid_imgs, size=self.large_size, mode='bilinear')
+
+        y3 = self.u_net(large_imgs)
+
+        with torch.no_grad():
+            y1 = self.u_net(small_imgs)
+            y2 = self.u_net(mid_imgs)
+
+
+        return y1, y2, y3
 
 
 class ResNet50(LightningModule):
@@ -23,33 +49,35 @@ class ResNet50(LightningModule):
         self.batch_size = batch_size
         self.weight_decay = weight_decay
         self.dataset_path = dataset_path
-        self.model = torchvision.models.resnet50(pretrained=False)
+        self.model = MultiScaleNet()
         self.criterion = nn.CrossEntropyLoss()
         self.train_acc = torchmetrics.Accuracy()
         self.val_acc = torchmetrics.Accuracy()
 
     def forward(self, x):
-        x = F.interpolate(x, size=224, mode='bilinear')
         return self.model(x)
 
     def training_step(self, batch, batch_idx):
         x, y = batch
-        y_hat = self(x)
-        loss = self.criterion(y_hat, y)
+        y_hat1, y_hat2, y_hat3 = self(x)
+        loss = self.criterion(y_hat3, y)
         self.log("train_loss", loss)
-        self.log("train_acc", self.train_acc(y_hat, y), on_epoch=True)
+        self.log("train_acc1", self.train_acc(y_hat1, y), on_epoch=True)
+        self.log("train_acc2", self.train_acc(y_hat2, y), on_epoch=True)
+        self.log("train_acc3", self.train_acc(y_hat3, y), on_epoch=True)
         return loss
 
     def validation_step(self, batch, batch_idx):
         x, y = batch
-        y_hat = self(x)
-        loss = self.criterion(y_hat, y)
+        y_hat1, y_hat2, y_hat3 = self(x)
+        loss = self.criterion(y_hat3, y)
         self.log("val_loss", loss)
-        self.log("val_acc", self.val_acc(y_hat, y), on_epoch=True)
+        self.log("val_acc1", self.val_acc(y_hat1, y), on_epoch=True)
+        self.log("val_acc2", self.val_acc(y_hat2, y), on_epoch=True)
+        self.log("val_acc3", self.val_acc(y_hat3, y), on_epoch=True)
 
     def configure_optimizers(self):
-        optimizer = torch.optim.SGD(self.parameters(), lr=self.learning_rate, weight_decay=self.weight_decay,
-                                    momentum=0.9)
+        optimizer = torch.optim.SGD(self.parameters(), lr=self.learning_rate, weight_decay=self.weight_decay,momentum=0.9)
         scheduler = LinearWarmupCosineAnnealingLR(
             optimizer,
             warmup_epochs=5,
@@ -95,13 +123,12 @@ if __name__ == "__main__":
     if args.resume_from_checkpoint is not None:
         trainer = Trainer.from_argparse_args(args, gpus=args.num_gpus, accelerator="ddp", logger=wandb_logger,
                                              callbacks=[checkpoint_callback, lr_monitor],
-                                             resume_from_checkpoint=args.resume_from_checkpoint, precision=16,
-                                             gradient_clip_val=1.0,
+                                             resume_from_checkpoint=args.resume_from_checkpoint, precision=16,gradient_clip_val=1.0,
                                              check_val_every_n_epoch=args.eval_every)
     else:
         trainer = Trainer.from_argparse_args(args, gpus=args.num_gpus, accelerator="ddp", logger=wandb_logger,
-                                             callbacks=[checkpoint_callback, lr_monitor], precision=16,
-                                             gradient_clip_val=1.0,
+                                             callbacks=[checkpoint_callback, lr_monitor], precision=16,gradient_clip_val=1.0,
                                              check_val_every_n_epoch=args.eval_every)
 
     trainer.fit(model)
+
