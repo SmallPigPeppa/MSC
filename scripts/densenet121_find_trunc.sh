07363c113af9ac6619637c90f908e8a6667b6677
lwz <905328134@qq.com>
2023-05-06 11:24:38 +0800

    initial

--- scripts/densenet121_find_trunc.sh
+++ scripts/densenet121_find_trunc.sh
@@ -1,19 +1,3 @@
-export https_proxy=http://10.7.4.2:3128
-cd /mnt/mmtech01/usr/liuwenzhuo/code/test-code/MSC-dali
-/root/miniconda3/envs/solo-learn/bin/python densenet121_l2.py \
---num_gpus 8 \
---weight_decay 2e-5 \
---project Multi-Scale-CNN-HPO \
---num_workers 8 \
---batch_size 128 \
---checkpoint_dir checkpoints/densenet121-l2 \
---run_name densenet121-l2-128 \
---max_epochs 90 \
---learning_rate 0.5 \
---trunc 0.001
-
-
-
 #export https_proxy=http://10.7.4.2:3128
 #cd /mnt/mmtech01/usr/liuwenzhuo/code/test-code/MSC-dali
 #/root/miniconda3/envs/solo-learn/bin/python densenet121_l2.py \
@@ -21,9 +5,25 @@ cd /mnt/mmtech01/usr/liuwenzhuo/code/test-code/MSC-dali
 #--weight_decay 2e-5 \
 #--project Multi-Scale-CNN-HPO \
 #--num_workers 8 \
-#--batch_size 32 \
+#--batch_size 128 \
 #--checkpoint_dir checkpoints/densenet121-l2 \
-#--run_name densenet121-l2 \
+#--run_name densenet121-l2-128 \
 #--max_epochs 90 \
-#--learning_rate 0.1 \
+#--learning_rate 0.5 \
 #--trunc 0.001
+
+
+
+export https_proxy=http://10.7.4.2:3128
+cd /mnt/mmtech01/usr/liuwenzhuo/code/test-code/MSC-dali
+/root/miniconda3/envs/solo-learn/bin/python densenet121_l2.py \
+--num_gpus 8 \
+--weight_decay 2e-5 \
+--project Multi-Scale-CNN-HPO \
+--num_workers 8 \
+--batch_size 32 \
+--checkpoint_dir checkpoints/densenet121-l2 \
+--run_name densenet121-l2 \
+--max_epochs 90 \
+--learning_rate 0.1 \
+--trunc 0.001
