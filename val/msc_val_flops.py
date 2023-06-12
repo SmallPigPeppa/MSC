4e896497cf97c583c7935025acd3778ec0f6cdf6
lwz <905328134@qq.com>
2023-06-12 17:52:21 +0800

    initial

--- val/msc_val_flops.py
+++ val/msc_val_flops.py
@@ -8,7 +8,7 @@ import torchmetrics
 from resnet50_l2_norm import ResNet50
 import torch.nn.functional as F
 from tqdm import tqdm
-import torchinfo
+from torchprofile import profile_macs
 
 
 def test_resolutions(model, dataset_path, resolutions):
@@ -50,9 +50,10 @@ def test_resolutions(model, dataset_path, resolutions):
                 correct3 += acc3.item() * inputs.size(0)
                 total += inputs.size(0)
 
-                if idx ==0:
-                    macs, params = torchinfo.summary(model, input_data=inputs, col_names=["macs"], verbose=0)
-                    flops = macs * 2
+                if idx == 0:
+                    macs = profile_macs(model, inputs)
+                    flops = macs / 1e9
+                    # GFLOPs
                     flops_list.append(flops)
 
         mean_acc1 = correct1 / total
@@ -63,7 +64,8 @@ def test_resolutions(model, dataset_path, resolutions):
         acc2_list.append(mean_acc2)
         acc3_list.append(mean_acc3)
         res_list.append(res)
-        print(f"Resolution: {res}, Accuracy1: {mean_acc1}, Accuracy2: {mean_acc2}, Accuracy3: {mean_acc3}, FLOPS:{flops_list}")
+        print(
+            f"Resolution: {res}, Accuracy1: {mean_acc1}, Accuracy2: {mean_acc2}, Accuracy3: {mean_acc3}, FLOPs:{flops_list}")
 
     return res_list, acc1_list, acc2_list, acc3_list, flops_list
 
@@ -76,7 +78,8 @@ if __name__ == "__main__":
 
     parser.add_argument("--run_name", type=str, default="resnet50-baseline", help="Name of the Weights & Biases run")
     parser.add_argument("--project", type=str, default="Multi-Scale-CNN", help="Name of the Weights & Biases project")
-    parser.add_argument("--entity", type=str, default='pigpeppa', help="Name of the Weights & Biases entity (team or user)")
+    parser.add_argument("--entity", type=str, default='pigpeppa',
+                        help="Name of the Weights & Biases entity (team or user)")
     parser.add_argument("--offline", action="store_true", help="Run Weights & Biases logger in offline mode")
     parser.add_argument("--learning_rate", type=float, default=0.1, help="Learning rate for the optimizer")
     parser.add_argument("--batch_size", type=int, default=32, help="Batch size for training")
@@ -87,20 +90,21 @@ if __name__ == "__main__":
     parser.add_argument("--num_gpus", type=int, default=8, help="Number of GPUs to use")
     parser.add_argument("--num_workers", type=int, default=8, help="Number of workers for data loading")
     parser.add_argument("--max_epochs", type=int, default=90, help="Maximum number of training epochs")
-    parser.add_argument("--dataset_path", type=str, default="/mnt/mmtech01/dataset/lzy/ILSVRC2012", help="Path to the ImageNet dataset")
+    parser.add_argument("--dataset_path", type=str, default="/mnt/mmtech01/dataset/lzy/ILSVRC2012",
+                        help="Path to the ImageNet dataset")
     parser.add_argument("--eval_every", type=int, default=5, help="Evaluate the model every N epochs")
     parser.add_argument("--trunc", type=float, default=0.01, help="trunc for the si loss")
-
 
     args = parser.parse_args()
 
     wandb.init(name=args.run_name, project=args.project, entity=args.entity)
     wandb_table = wandb.Table(columns=["Resolution", "Accuracy"])
 
-    model = ResNet50.load_from_checkpoint(args.checkpoint_path,args=args)
+    model = ResNet50.load_from_checkpoint(args.checkpoint_path, args=args)
 
     resolutions = list(range(32, 225, 16))
-    res_list, acc1_list, acc2_list, acc3_list,flops_list = test_resolutions(model, args.dataset_path, resolutions)
+    res_list, acc1_list, acc2_list, acc3_list, flops_list = test_resolutions(model, args.dataset_path, resolutions)
 
-    wandb.log({"Resolution": res_list, "Accuracy1": acc1_list, "Accuracy2": acc2_list, "Accuracy3": acc3_list,"FLOPS": flops_list})
+    wandb.log({"Resolution": res_list, "Accuracy1": acc1_list, "Accuracy2": acc2_list, "Accuracy3": acc3_list,
+               "FLOPs": flops_list})
     wandb.finish()
