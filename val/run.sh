0d56278c320959058ff1c38804d2d5ce5d394d5f
lwz <905328134@qq.com>
2023-06-12 13:28:25 +0800

    initial

--- val/run.sh
+++ val/run.sh
@@ -1,4 +1,5 @@
 export https_proxy=http://10.7.4.2:3128
+export PYTHONPATH=/mnt/mmtech01/usr/liuwenzhuo/code/test-code/MSC-dali
 /root/miniconda3/envs/solo-learn/bin/python  msc_val_flops.py \
   --checkpoint_path /mnt/mmtech01/usr/liuwenzhuo/code/test-code/MSC-dali/checkpoints/resnet50-l2-norm/last.ckpt\
   --dataset_path /mnt/mmtech01/dataset/lzy/ILSVRC2012 \
