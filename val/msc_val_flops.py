e20f97e8d0082d6414bd3b6fa22779cbc2998002
lwz <905328134@qq.com>
2023-06-12 13:30:50 +0800

    initial

--- val/msc_val_flops.py
+++ val/msc_val_flops.py
@@ -73,20 +73,31 @@ if __name__ == "__main__":
     parser.add_argument("--checkpoint_path", type=str,
                         default='checkpoints/resnet50-baseline-s-clip-multi/epoch=89-step=112680.ckpt',
                         help="Path to the trained model checkpoint")
-    parser.add_argument("--dataset_path", type=str, default="/mnt/mmtech01/dataset/lzy/ILSVRC2012",
-                        help="Path to the ImageNet dataset")
-    parser.add_argument("--project", type=str, default="Multi-scale-CNN val",
-                        help="Name of the Weights & Biases project")
-    parser.add_argument("--entity", type=str, default="pigpeppa",
-                        help="Name of the Weights & Biases entity (team or user)")
-    parser.add_argument("--run_name", type=str, default="resnet50-debug", help="Name of the Weights & Biases run")
+
+    parser.add_argument("--run_name", type=str, default="resnet50-baseline", help="Name of the Weights & Biases run")
+    parser.add_argument("--project", type=str, default="Multi-Scale-CNN", help="Name of the Weights & Biases project")
+    parser.add_argument("--entity", type=str, default='pigpeppa', help="Name of the Weights & Biases entity (team or user)")
+    parser.add_argument("--offline", action="store_true", help="Run Weights & Biases logger in offline mode")
+    parser.add_argument("--learning_rate", type=float, default=0.1, help="Learning rate for the optimizer")
+    parser.add_argument("--batch_size", type=int, default=32, help="Batch size for training")
+    parser.add_argument("--weight_decay", type=float, default=1e-4, help="Weight decay for the optimizer")
+    parser.add_argument("--checkpoint_dir", type=str, default="./checkpoints", help="Directory to save checkpoints")
+    parser.add_argument("--resume_from_checkpoint", type=str, default=None,
+                        help="Path to checkpoint to resume training from")
+    parser.add_argument("--num_gpus", type=int, default=8, help="Number of GPUs to use")
+    parser.add_argument("--num_workers", type=int, default=8, help="Number of workers for data loading")
+    parser.add_argument("--max_epochs", type=int, default=90, help="Maximum number of training epochs")
+    parser.add_argument("--dataset_path", type=str, default="/mnt/mmtech01/dataset/lzy/ILSVRC2012", help="Path to the ImageNet dataset")
+    parser.add_argument("--eval_every", type=int, default=5, help="Evaluate the model every N epochs")
+    parser.add_argument("--trunc", type=float, default=0.01, help="trunc for the si loss")
+
 
     args = parser.parse_args()
 
     wandb.init(name=args.run_name, project=args.project, entity=args.entity)
     wandb_table = wandb.Table(columns=["Resolution", "Accuracy"])
 
-    model = ResNet50.load_from_checkpoint(args.checkpoint_path)
+    model = ResNet50.load_from_checkpoint(args.checkpoint_path,args=args)
 
     resolutions = list(range(32, 225, 16))
     res_list, acc1_list, acc2_list, acc3_list,flops_list = test_resolutions(model, args.dataset_path, resolutions)
