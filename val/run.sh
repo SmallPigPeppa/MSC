03ed70c739d3535a5798eda0f9aae556e8c4ef67
lwz <905328134@qq.com>
2023-06-15 17:26:38 +0800

    initial

--- val/run.sh
+++ val/run.sh
@@ -1,7 +1,7 @@
 export https_proxy=http://10.7.4.2:3128
 export PYTHONPATH=/mnt/mmtech01/usr/liuwenzhuo/code/test-code/MSC-dali
 /root/miniconda3/envs/solo-learn/bin/python  msc_val_flops.py \
-  --checkpoint_path /mnt/mmtech01/usr/liuwenzhuo/code/test-code/MSC-dali/checkpoints/resnet50-l2-norm/last.ckpt\
+  --checkpoint_path /mnt/mmtech01/usr/liuwenzhuo/code/test-code/MSC-dali/checkpoints/resnet50-l2-last/last.ckpt\
   --dataset_path /mnt/mmtech01/dataset/lzy/ILSVRC2012 \
   --project msc-val\
   --run_name resnet50-l2\
