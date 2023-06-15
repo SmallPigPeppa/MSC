46d4c7eeee45b02929d096ce90b65b24f2bedc29
lwz <905328134@qq.com>
2023-06-15 20:30:09 +0800

    initial

--- val/run_baseline_all.sh
+++ val/run_baseline_all.sh
@@ -7,25 +7,66 @@ export PYTHONPATH=/mnt/mmtech01/usr/liuwenzhuo/code/test-code/MSC-dali
 #  --run_name resnet50-baseline\
 #  --method resnet50
 
+#/root/miniconda3/envs/solo-learn/bin/python  msc_val_baseline_all.py \
+#  --checkpoint_path /mnt/mmtech01/usr/liuwenzhuo/code/test-code/MSC-dali/baseline/checkpoints/vgg16-bn-baseline/last.ckpt\
+#  --dataset_path /mnt/mmtech01/dataset/lzy/ILSVRC2012 \
+#  --project msc-val\
+#  --run_name vgg16-bn-baseline\
+#  --method vgg16_bn
+#
+#
+#/root/miniconda3/envs/solo-learn/bin/python  msc_val_baseline_all.py \
+#  --checkpoint_path /mnt/mmtech01/usr/liuwenzhuo/code/test-code/MSC-dali/baseline/checkpoints/densenet121-baseline/last.ckpt\
+#  --dataset_path /mnt/mmtech01/dataset/lzy/ILSVRC2012 \
+#  --project msc-val\
+#  --run_name densenet121-baseline\
+#  --method densenet121
+#
+#/root/miniconda3/envs/solo-learn/bin/python  msc_val_baseline_all.py \
+#  --checkpoint_path /mnt/mmtech01/usr/liuwenzhuo/code/test-code/MSC-dali/baseline/checkpoints/mobilenetv2-baseline/last.ckpt\
+#  --dataset_path /mnt/mmtech01/dataset/lzy/ILSVRC2012 \
+#  --project msc-val\
+#  --run_name mobilenetv2-baseline\
+#  --method mobilenetv2
+
+#      elif args.method == 'resnext50':
+#        from torchvision.models import resnext50_32x4d
+#        model = resnext50_32x4d(pretrained=True)
+#    elif args.method == 'googlenet':
+#        from torchvision.models import googlenet
+#        model = googlenet(pretrained=True)
+#    elif args.method == 'inceptionv3':
+#        from torchvision.models import inception_v3
+#        model = inception_v3(pretrained=True)
+#    elif args.method == 'alexnet':
+
+
 /root/miniconda3/envs/solo-learn/bin/python  msc_val_baseline_all.py \
   --checkpoint_path /mnt/mmtech01/usr/liuwenzhuo/code/test-code/MSC-dali/baseline/checkpoints/vgg16-bn-baseline/last.ckpt\
   --dataset_path /mnt/mmtech01/dataset/lzy/ILSVRC2012 \
   --project msc-val\
-  --run_name vgg16-bn-baseline\
-  --method vgg16_bn
+  --run_name resnext50-baseline\
+  --method resnext50
 
 
 /root/miniconda3/envs/solo-learn/bin/python  msc_val_baseline_all.py \
   --checkpoint_path /mnt/mmtech01/usr/liuwenzhuo/code/test-code/MSC-dali/baseline/checkpoints/densenet121-baseline/last.ckpt\
   --dataset_path /mnt/mmtech01/dataset/lzy/ILSVRC2012 \
   --project msc-val\
-  --run_name densenet121-baseline\
-  --method densenet121
+  --run_name googlenet-baseline\
+  --method googlenet
 
 /root/miniconda3/envs/solo-learn/bin/python  msc_val_baseline_all.py \
   --checkpoint_path /mnt/mmtech01/usr/liuwenzhuo/code/test-code/MSC-dali/baseline/checkpoints/mobilenetv2-baseline/last.ckpt\
   --dataset_path /mnt/mmtech01/dataset/lzy/ILSVRC2012 \
   --project msc-val\
-  --run_name mobilenetv2-baseline\
-  --method mobilenetv2
+  --run_name nceptionv3-baseline\
+  --method nceptionv3
+
+/root/miniconda3/envs/solo-learn/bin/python  msc_val_baseline_all.py \
+  --checkpoint_path /mnt/mmtech01/usr/liuwenzhuo/code/test-code/MSC-dali/baseline/checkpoints/mobilenetv2-baseline/last.ckpt\
+  --dataset_path /mnt/mmtech01/dataset/lzy/ILSVRC2012 \
+  --project msc-val\
+  --run_name alexnet-baseline\
+  --method alexnet
 
