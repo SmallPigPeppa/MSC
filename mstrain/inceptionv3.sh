94b893c158233cb3e56afdf4f61a2b73e48c4778
lwz <905328134@qq.com>
2023-06-19 16:11:06 +0800

    initial

--- mstrain/inceptionv3.sh
+++ mstrain/inceptionv3.sh
@@ -2,12 +2,12 @@ export https_proxy=http://10.7.4.2:3128
 cd /mnt/mmtech01/usr/liuwenzhuo/code/test-code/MSC-dali/mstrain
 /root/miniconda3/envs/solo-learn/bin/python inceptionv3.py \
 --num_gpus 8 \
---weight_decay 2e-5 \
+--weight_decay 1e-4 \
 --project Multi-Scale-CNN-HPO \
 --num_workers 8 \
 --batch_size 32 \
 --checkpoint_dir checkpoints/inceptionv3-mstrain \
 --run_name inceptionv3-mstrain \
 --max_epochs 90 \
---learning_rate 0.1 \
+--learning_rate 1e-2 \
 --trunc 0.001
