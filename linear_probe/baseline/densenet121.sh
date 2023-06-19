59ba40280117c4f751b75bcb0bbc4dcf1826d5e3
lwz <905328134@qq.com>
2023-06-19 22:55:52 +0800

    initial

--- linear_probe/baseline/densenet121.sh
+++ linear_probe/baseline/densenet121.sh
@@ -5,7 +5,7 @@ export https_proxy=http://10.7.4.2:3128
   --project msc-transfer-linear \
   --num_workers 8 \
   --batch_size 32 \
-  --dataset_path /mnt/mmtech01/dataset/lzy/ILSVRC2012 \
+  --dataset_path /mnt/mmtech01/usr/liuwenzhuo/torch_ds/ \
   --checkpoint_path /mnt/mmtech01/usr/liuwenzhuo/code/test-code/MSC-dali/baseline/checkpoints/densenet121-baseline/last.ckpt \
   --run_name densenet121 \
   --max_epochs 90 \
