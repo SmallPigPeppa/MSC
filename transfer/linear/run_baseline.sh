9671572ab2f8fd8ca1b231480bf98d95354c8278
lwz <905328134@qq.com>
2023-06-20 20:11:21 +0800

    initial

--- transfer/linear/run_baseline.sh
+++ transfer/linear/run_baseline.sh
@@ -4,7 +4,8 @@ export https_proxy=http://10.7.4.2:3128
 
 # 设置要运行的数据集
 #datasets=("cifar10" "cifar100" "stl10" "caltech" "fashion" "flowers")
-datasets=( "stl10" "caltech" "fashion" "flowers")
+#datasets=( "stl10" "caltech" "fashion" "flowers")
+datasets=("caltech" "fashion")
 
 # 设置你要运行的模型及其对应的脚本和检查点路径
 declare -A models
@@ -22,25 +23,25 @@ gpus["mobilenetv2"]="6,7"
 
 # 迭代不同的数据集
 for dataset in "${datasets[@]}"; do
-    # 迭代不同的模型
-    for model in "${!models[@]}"; do
-        checkpoint=${models[$model]}
-        script="main_baseline.py"
-        gpu=${gpus[$model]}
-        # 运行脚本
-        CUDA_VISIBLE_DEVICES=$gpu /root/miniconda3/envs/solo-learn/bin/python $script \
-            --num_gpus 2 \
-            --weight_decay 2e-5 \
-            --project msc-transfer-linear \
-            --num_workers 8 \
-            --batch_size 32 \
-            --dataset_path /mnt/mmtech01/usr/liuwenzhuo/torch_ds/ \
-            --checkpoint_path $checkpoint \
-            --run_name baseline \
-            --max_epochs 90 \
-            --learning_rate 0.1 \
-            --model $model \
-            --dataset $dataset &
-    done
-    wait
+  # 迭代不同的模型
+  for model in "${!models[@]}"; do
+    checkpoint=${models[$model]}
+    script="main_baseline.py"
+    gpu=${gpus[$model]}
+    # 运行脚本
+    CUDA_VISIBLE_DEVICES=$gpu /root/miniconda3/envs/solo-learn/bin/python $script \
+      --num_gpus 2 \
+      --weight_decay 2e-5 \
+      --project msc-transfer-linear \
+      --num_workers 8 \
+      --batch_size 32 \
+      --dataset_path /mnt/mmtech01/usr/liuwenzhuo/torch_ds/ \
+      --checkpoint_path $checkpoint \
+      --run_name baseline \
+      --max_epochs 90 \
+      --learning_rate 0.1 \
+      --model $model \
+      --dataset $dataset &
+  done
+  wait
 done
