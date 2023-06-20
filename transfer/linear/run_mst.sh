2740ac03cea79140e0f5018becfe89f71cea2b78
lwz <905328134@qq.com>
2023-06-20 15:47:09 +0800

    initial

--- transfer/linear/run_mst.sh
+++ transfer/linear/run_mst.sh
@@ -4,7 +4,8 @@ export https_proxy=http://10.7.4.2:3128
 export PYTHONPATH=/mnt/mmtech01/usr/liuwenzhuo/code/test-code/MSC-dali
 
 # 设置要运行的数据集
-datasets=("cifar10" "cifar100" "stl10")
+#datasets=("cifar10" "cifar100" "stl10" "caltech" "fashion" "flowers")
+datasets=( "stl10" "caltech" "fashion" "flowers")
 
 # 设置你要运行的模型及其对应的脚本和检查点路径
 declare -A models
