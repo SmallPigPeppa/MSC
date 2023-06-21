df0fbb85ee1256cf161846b34f64ea9d472704f1
lwz <905328134@qq.com>
2023-06-21 23:51:08 +0800

    initial

--- transfer/linear_msc/run_msc_debug.sh
+++ transfer/linear_msc/run_msc_debug.sh
@@ -5,12 +5,11 @@ export PYTHONPATH=/mnt/mmtech01/usr/liuwenzhuo/code/test-code/MSC-dali
 
 # 设置要运行的数据集
 #datasets=("cifar10" "cifar100" "stl10" "caltech" "fashion" "flowers" "pets" "cars" "rafdb" "aircraft" "dtd")
-datasets=( "cifar10")
-
+datasets=("cifar10")
 
 # 设置你要运行的模型及其对应的脚本和检查点路径
 declare -A models
-models["resnet50"]="/mnt/mmtech01/usr/liuwenzhuo/code/test-code/MSC-dali/mstrain/checkpoints/resnet50-mstrain/last.ckpt"
+models["resnet50"]="/mnt/mmtech01/usr/liuwenzhuo/code/test-code/MSC-dali/checkpoints/resnet50-l2-last/last.ckpt"
 #models["densenet121"]="/mnt/mmtech01/usr/liuwenzhuo/code/test-code/MSC-dali/mstrain/checkpoints/densenet121-mstrain/last.ckpt"
 #models["vgg16"]="/mnt/mmtech01/usr/liuwenzhuo/code/test-code/MSC-dali/mstrain/checkpoints/vgg16-bn-mstrain/last.ckpt"
 #models["mobilenetv2"]="/mnt/mmtech01/usr/liuwenzhuo/code/test-code/MSC-dali/mstrain/checkpoints/mobilenetv2-mstrain/last.ckpt"
@@ -24,25 +23,25 @@ gpus["resnet50"]="0,1"
 
 # 迭代不同的数据集
 for dataset in "${datasets[@]}"; do
-    # 迭代不同的模型
-    for model in "${!models[@]}"; do
-        checkpoint=${models[$model]}
-        script="main_msc.py"
-        gpu=${gpus[$model]}
-        # 运行脚本
-        CUDA_VISIBLE_DEVICES=$gpu /root/miniconda3/envs/solo-learn/bin/python $script \
-            --num_gpus 2 \
-            --weight_decay 2e-5 \
-            --project msc-transfer-linear-debug \
-            --num_workers 8 \
-            --batch_size 32 \
-            --dataset_path /mnt/mmtech01/usr/liuwenzhuo/torch_ds/ \
-            --checkpoint_path $checkpoint \
-            --run_name msc \
-            --max_epochs 90 \
-            --learning_rate 0.1 \
-            --model $model \
-            --dataset $dataset &
-    done
-    wait
+  # 迭代不同的模型
+  for model in "${!models[@]}"; do
+    checkpoint=${models[$model]}
+    script="main_msc.py"
+    gpu=${gpus[$model]}
+    # 运行脚本
+    CUDA_VISIBLE_DEVICES=$gpu /root/miniconda3/envs/solo-learn/bin/python $script \
+      --num_gpus 2 \
+      --weight_decay 2e-5 \
+      --project msc-transfer-linear-debug \
+      --num_workers 8 \
+      --batch_size 32 \
+      --dataset_path /mnt/mmtech01/usr/liuwenzhuo/torch_ds/ \
+      --checkpoint_path $checkpoint \
+      --run_name msc \
+      --max_epochs 90 \
+      --learning_rate 0.1 \
+      --model $model \
+      --dataset $dataset &
+  done
+  wait
 done
